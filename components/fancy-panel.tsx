6412cf50d8dc0b5bc50dd8fd143651a2d69b8c19
Mikołaj Pocheć <mikolaj_pochec@protonmail.com>
2026-05-04 12:48:32 +0200

    Add FancyPanel

---
+++ components/fancy-panel.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import styles from "../styles/fancy-panel.module.css";
+
+export default function FancyPanel({
+	maxTilt = 12,
+	glowColor = "white",
+	className = "",
+	children,
+}: {
+	maxTilt?: number;
+	glowColor?: string;
+	className?: string;
+	children: React.ReactNode;
+}) {
+	const handleMouseMove = (e: any) => {
+		const el = e.currentTarget;
+		const r = el.getBoundingClientRect();
+		const x = e.clientX - r.left;
+		const y = e.clientY - r.top;
+		const nx = (x / r.width - 0.5) * 2;
+		const ny = (y / r.height - 0.5) * 2;
+		el.style.setProperty("--x", x);
+		el.style.setProperty("--y", y);
+		el.style.setProperty("--rx", ny * maxTilt);
+		el.style.setProperty("--ry", nx * maxTilt);
+	};
+
+	const handleMouseLeave = (e: any) => {
+		const el = e.currentTarget;
+		el.style.setProperty("--x", -Infinity);
+		el.style.setProperty("--y", -Infinity);
+		el.style.setProperty("--rx", 0);
+		el.style.setProperty("--ry", 0);
+	};
+
+	return (
+		<div
+			className={`${styles.panel} ${className}`}
+			style={{ "--glow-color": glowColor } as React.CSSProperties}
+			onMouseMove={handleMouseMove}
+			onMouseLeave={handleMouseLeave}
+		>
+			{children}
+		</div>
+	);
+}
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,16 +1,20 @@
 import TypewriterHeader from "../components/typewriter-header";
 import ReactiveGrid from "../components/reactive-grid";
+import FancyPanel from "../components/fancy-panel";
 
 export default function Hero() {
 	return (
 		<div className="relative h-full w-full p-32 flex items-center bg-white dark:bg-black">
 			<div className="flex gap-5 flex-col z-2">
 				<TypewriterHeader targetText="Mikołaj" />
-				<p className="text-gray-400 max-w-1/2 text-justify">
+				<p className="text-gray-300 max-w-1/2 text-justify">
 					This is an eigenspace of Mikołaj Pocheć, a computer science student
 					based in Cracow, Poland. I'm into software development, cybersecurity
 					and low level programming.
 				</p>
+				<FancyPanel glowColor="#00ffff" className="p-4 w-fit" maxTilt={12}>
+					Lorem ipsum dolor sit amet
+				</FancyPanel>
 			</div>
 			<div className="absolute top-0 right-0 h-full w-1/2 invert dark:invert-0 z-1">
 				<ReactiveGrid cursorRepelForce={2.5} />
